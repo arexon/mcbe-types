bdc940aef52d28540723b132c5592c98a5a0fca9
arexon <arexonreal@gmail.com>
2025-12-06 22:47:29 +0200

    chore: init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          deno
+          vscode-json-languageserver
+        ];
+      };
+    });
+}
