ec52d0c848d2970f5b50bc8a6f9b011799789400
arexon <arexonreal@gmail.com>
2025-12-12 03:57:28 +0200

    feat: basic serialization library

---
+++ serialize/mod.ts
@@ -0,0 +1,94 @@
+import { toSnakeCase } from "@std/text";
+
+export interface SerContext {
+    readonly metadata: {
+        fields?: Map<string, SerFieldOptions | undefined>;
+    };
+}
+
+export interface SerFieldOptions {
+    default?: () => unknown;
+}
+
+export function SerField<Value>(opts?: SerFieldOptions): (
+    _target: undefined,
+    ctx: ClassFieldDecoratorContext<unknown, Value> & SerContext,
+) => void {
+    return function (_target, ctx) {
+        if (ctx.metadata.fields === undefined) {
+            ctx.metadata.fields = new Map();
+        }
+        if (typeof ctx.name !== "symbol") {
+            ctx.metadata.fields.set(ctx.name, opts);
+        }
+    };
+}
+
+export interface SerClassOptions<T> {
+    transparent?: T;
+}
+
+export function SerClass<
+    // deno-lint-ignore no-explicit-any
+    T extends abstract new (...args: any[]) => InstanceType<T>,
+    TInstance extends InstanceType<T>,
+    Field extends {
+        // deno-lint-ignore ban-types
+        [P in keyof TInstance]: TInstance[P] extends Function ? never : P;
+    }[keyof TInstance],
+>(
+    classOpts?: SerClassOptions<Field>,
+): (
+    target: T,
+    ctx: ClassDecoratorContext & SerContext,
+) => void {
+    return function (target, ctx) {
+        // TODO: Experiment with generating `Function`.
+        Object.defineProperty(target.prototype, "toJSON", {
+            value() {
+                interface InspectedField {
+                    name: string;
+                    opts?: SerFieldOptions;
+                    isDefault: boolean;
+                    isUndefined: boolean;
+                    isTransparent: boolean;
+                }
+                const inspectedFields = ctx
+                    .metadata
+                    .fields
+                    ?.entries()
+                    .map(([fieldName, fieldOpts]): InspectedField => ({
+                        name: fieldName,
+                        opts: fieldOpts,
+                        isDefault: fieldOpts !== undefined &&
+                            fieldOpts.default !== undefined &&
+                            fieldOpts.default() === this[fieldName],
+                        isUndefined: this[fieldName] === undefined,
+                        isTransparent: classOpts?.transparent === fieldName,
+                    }))
+                    .toArray() ?? [];
+
+                if (
+                    typeof classOpts?.transparent === "string" &&
+                    inspectedFields.every((field) =>
+                        field.isTransparent ||
+                        field.isUndefined ||
+                        field.isDefault
+                    )
+                ) {
+                    return this[classOpts.transparent];
+                } else {
+                    const object: Record<string, unknown> = {};
+                    for (const field of inspectedFields) {
+                        let value = this[field.name];
+                        if (field.isDefault) value = undefined;
+                        object[toSnakeCase(field.name)] = value;
+                    }
+                    return object;
+                }
+            },
+            configurable: true,
+            writable: true,
+        });
+    };
+}
