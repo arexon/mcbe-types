3474560c867b05d4d472b0259dd5bb84821a63e4
arexon <arexonreal@gmail.com>
2025-12-13 03:13:43 +0200

    chore: use default fmt settings

--- serialize/mod.ts
+++ serialize/mod.ts
@@ -1,94 +1,94 @@
 import { toSnakeCase } from "@std/text";
 
 export interface SerContext {
-    readonly metadata: {
-        fields?: Map<string, SerFieldOptions | undefined>;
-    };
+  readonly metadata: {
+    fields?: Map<string, SerFieldOptions | undefined>;
+  };
 }
 
 export interface SerFieldOptions {
-    default?: () => unknown;
+  default?: () => unknown;
 }
 
 export function SerField<Value>(opts?: SerFieldOptions): (
-    _target: undefined,
-    ctx: ClassFieldDecoratorContext<unknown, Value> & SerContext,
+  _target: undefined,
+  ctx: ClassFieldDecoratorContext<unknown, Value> & SerContext,
 ) => void {
-    return function (_target, ctx) {
-        if (ctx.metadata.fields === undefined) {
-            ctx.metadata.fields = new Map();
-        }
-        if (typeof ctx.name !== "symbol") {
-            ctx.metadata.fields.set(ctx.name, opts);
-        }
-    };
+  return function (_target, ctx) {
+    if (ctx.metadata.fields === undefined) {
+      ctx.metadata.fields = new Map();
+    }
+    if (typeof ctx.name !== "symbol") {
+      ctx.metadata.fields.set(ctx.name, opts);
+    }
+  };
 }
 
 export interface SerClassOptions<T> {
-    transparent?: T;
+  transparent?: T;
 }
 
 export function SerClass<
-    // deno-lint-ignore no-explicit-any
-    T extends abstract new (...args: any[]) => InstanceType<T>,
-    TInstance extends InstanceType<T>,
-    Field extends {
-        // deno-lint-ignore ban-types
-        [P in keyof TInstance]: TInstance[P] extends Function ? never : P;
-    }[keyof TInstance],
+  // deno-lint-ignore no-explicit-any
+  T extends abstract new (...args: any[]) => InstanceType<T>,
+  TInstance extends InstanceType<T>,
+  Field extends {
+    // deno-lint-ignore ban-types
+    [P in keyof TInstance]: TInstance[P] extends Function ? never : P;
+  }[keyof TInstance],
 >(
-    classOpts?: SerClassOptions<Field>,
+  classOpts?: SerClassOptions<Field>,
 ): (
-    target: T,
-    ctx: ClassDecoratorContext & SerContext,
+  target: T,
+  ctx: ClassDecoratorContext & SerContext,
 ) => void {
-    return function (target, ctx) {
-        // TODO: Experiment with generating `Function`.
-        Object.defineProperty(target.prototype, "toJSON", {
-            value() {
-                interface InspectedField {
-                    name: string;
-                    opts?: SerFieldOptions;
-                    isDefault: boolean;
-                    isUndefined: boolean;
-                    isTransparent: boolean;
-                }
-                const inspectedFields = ctx
-                    .metadata
-                    .fields
-                    ?.entries()
-                    .map(([fieldName, fieldOpts]): InspectedField => ({
-                        name: fieldName,
-                        opts: fieldOpts,
-                        isDefault: fieldOpts !== undefined &&
-                            fieldOpts.default !== undefined &&
-                            fieldOpts.default() === this[fieldName],
-                        isUndefined: this[fieldName] === undefined,
-                        isTransparent: classOpts?.transparent === fieldName,
-                    }))
-                    .toArray() ?? [];
+  return function (target, ctx) {
+    // TODO: Experiment with generating `Function`.
+    Object.defineProperty(target.prototype, "toJSON", {
+      value() {
+        interface InspectedField {
+          name: string;
+          opts?: SerFieldOptions;
+          isDefault: boolean;
+          isUndefined: boolean;
+          isTransparent: boolean;
+        }
+        const inspectedFields = ctx
+          .metadata
+          .fields
+          ?.entries()
+          .map(([fieldName, fieldOpts]): InspectedField => ({
+            name: fieldName,
+            opts: fieldOpts,
+            isDefault: fieldOpts !== undefined &&
+              fieldOpts.default !== undefined &&
+              fieldOpts.default() === this[fieldName],
+            isUndefined: this[fieldName] === undefined,
+            isTransparent: classOpts?.transparent === fieldName,
+          }))
+          .toArray() ?? [];
 
-                if (
-                    typeof classOpts?.transparent === "string" &&
-                    inspectedFields.every((field) =>
-                        field.isTransparent ||
-                        field.isUndefined ||
-                        field.isDefault
-                    )
-                ) {
-                    return this[classOpts.transparent];
-                } else {
-                    const object: Record<string, unknown> = {};
-                    for (const field of inspectedFields) {
-                        let value = this[field.name];
-                        if (field.isDefault) value = undefined;
-                        object[toSnakeCase(field.name)] = value;
-                    }
-                    return object;
-                }
-            },
-            configurable: true,
-            writable: true,
-        });
-    };
+        if (
+          typeof classOpts?.transparent === "string" &&
+          inspectedFields.every((field) =>
+            field.isTransparent ||
+            field.isUndefined ||
+            field.isDefault
+          )
+        ) {
+          return this[classOpts.transparent];
+        } else {
+          const object: Record<string, unknown> = {};
+          for (const field of inspectedFields) {
+            let value = this[field.name];
+            if (field.isDefault) value = undefined;
+            object[toSnakeCase(field.name)] = value;
+          }
+          return object;
+        }
+      },
+      configurable: true,
+      writable: true,
+    });
+  };
 }
--- serialize/mod_test.ts
+++ serialize/mod_test.ts
@@ -2,100 +2,100 @@ import { SerClass, SerField } from "@mcbe/serialize";
 import { assertEquals } from "@std/assert";
 
 Deno.test("basic serialization", () => {
-    @SerClass()
-    class Foo {
-        @SerField()
-        a: number;
+  @SerClass()
+  class Foo {
+    @SerField()
+    a: number;
 
-        @SerField()
-        b?: string;
+    @SerField()
+    b?: string;
 
-        constructor() {
-            this.a = 8;
-        }
+    constructor() {
+      this.a = 8;
     }
+  }
 
-    assertEquals(
-        JSON.stringify(new Foo()),
-        `{"a":8}`,
-        "should serialize",
-    );
+  assertEquals(
+    JSON.stringify(new Foo()),
+    `{"a":8}`,
+    "should serialize",
+  );
 });
 
 Deno.test("serialization with defaults", () => {
-    @SerClass()
-    class Foo {
-        @SerField()
-        a: number;
+  @SerClass()
+  class Foo {
+    @SerField()
+    a: number;
 
-        @SerField({ default: () => "hi" })
-        b: string;
+    @SerField({ default: () => "hi" })
+    b: string;
 
-        constructor() {
-            this.a = 8;
-            this.b = "hi";
-        }
+    constructor() {
+      this.a = 8;
+      this.b = "hi";
     }
+  }
 
-    const foo = new Foo();
-    assertEquals(
-        JSON.stringify(foo),
-        `{"a":8}`,
-        "should skip since field is undefined",
-    );
+  const foo = new Foo();
+  assertEquals(
+    JSON.stringify(foo),
+    `{"a":8}`,
+    "should skip since field is undefined",
+  );
 
-    foo.b = "hey";
-    assertEquals(
-        JSON.stringify(foo),
-        `{"a":8,"b":"hey"}`,
-        "should not skip since field does not match default value",
-    );
+  foo.b = "hey";
+  assertEquals(
+    JSON.stringify(foo),
+    `{"a":8,"b":"hey"}`,
+    "should not skip since field does not match default value",
+  );
 
-    foo.b = "hi";
-    assertEquals(
-        JSON.stringify(foo),
-        `{"a":8}`,
-        "should skip since field matches default value",
-    );
+  foo.b = "hi";
+  assertEquals(
+    JSON.stringify(foo),
+    `{"a":8}`,
+    "should skip since field matches default value",
+  );
 });
 
 Deno.test("serialization with transparency", () => {
-    @SerClass({ transparent: "a" })
-    class Foo {
-        @SerField()
-        a: string;
+  @SerClass({ transparent: "a" })
+  class Foo {
+    @SerField()
+    a: string;
 
-        @SerField()
-        b?: number;
+    @SerField()
+    b?: number;
 
-        @SerField({ default: () => true })
-        c: boolean;
+    @SerField({ default: () => true })
+    c: boolean;
 
-        constructor() {
-            this.a = "apple";
-            this.c = true;
-        }
+    constructor() {
+      this.a = "apple";
+      this.c = true;
     }
+  }
 
-    const foo = new Foo();
-    assertEquals(
-        JSON.stringify(foo),
-        `"apple"`,
-        "should directly serialize field since other fields are undefined",
-    );
+  const foo = new Foo();
+  assertEquals(
+    JSON.stringify(foo),
+    `"apple"`,
+    "should directly serialize field since other fields are undefined",
+  );
 
-    foo.b = 3;
-    assertEquals(
-        JSON.stringify(foo),
-        `{"a":"apple","b":3}`,
-        "should serialize entire object since all fields are present",
-    );
+  foo.b = 3;
+  assertEquals(
+    JSON.stringify(foo),
+    `{"a":"apple","b":3}`,
+    "should serialize entire object since all fields are present",
+  );
 
-    foo.b = undefined;
-    foo.c = false;
-    assertEquals(
-        JSON.stringify(foo),
-        `{"a":"apple","c":false}`,
-        "should serialize entire object since field does not match default value",
-    );
+  foo.b = undefined;
+  foo.c = false;
+  assertEquals(
+    JSON.stringify(foo),
+    `{"a":"apple","c":false}`,
+    "should serialize entire object since field does not match default value",
+  );
 });
