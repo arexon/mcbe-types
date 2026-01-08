334d524f4a0261bac72706bf3886f82986483cac
arexon <arexonreal@gmail.com>
2026-01-08 13:40:19 +0200

    refactor packs

--- types/packs.ts
+++ types/packs.ts
@@ -13,8 +13,8 @@ export type BehaviorPackDefinition =
 export class BehaviorPack {
   rootPath: string;
 
-  blocks: Map<string, Block> = new Map();
-  items: Map<string, Item> = new Map();
+  blocks: DefinitionSet<Block> = new DefinitionSet(Block);
+  items: DefinitionSet<Item> = new DefinitionSet(Item);
   manifest: Manifest;
 
   constructor(rootPath: string, manifest: Manifest) {
@@ -22,31 +22,98 @@ export class BehaviorPack {
     this.manifest = manifest;
   }
 
-  set(...defs: BehaviorPackDefinition[]): void {
+  add(...defs: BehaviorPackDefinition[]): void {
     for (const def of defs) {
       if (def instanceof Block) {
-        this.blocks.set(idToPath(def.identifier), def);
+        this.blocks.add(def);
       } else if (def instanceof Item) {
-        this.items.set(idToPath(def.identifier), def);
+        this.items.add(def);
       }
     }
   }
 
   async save(): Promise<void> {
-    {
-      const path = join(this.rootPath, "manifest.json");
-      await writeJsonFile(path, this.manifest);
-    }
+    const tasks: Promise<void>[] = [];
 
-    for (const [path, block] of this.blocks) {
-      const fullPath = join(this.rootPath, "blocks", path + ".block.json");
-      await writeJsonFile(fullPath, block);
-    }
+    tasks.push(
+      writeJsonFile(join(this.rootPath, "manifest.json"), this.manifest),
+    );
 
-    for (const [path, item] of this.items) {
-      const fullPath = join(this.rootPath, "items", path + ".item.json");
-      await writeJsonFile(fullPath, item);
+    tasks.push(
+      ...this.blocks.values().map((def) => {
+        const path = this.#createPath(
+          "blocks",
+          def.identifier,
+          ".block.json",
+        );
+        return writeJsonFile(path, def);
+      }),
+    );
+
+    tasks.push(
+      ...this.items.values().map((def) => {
+        const path = this.#createPath(
+          "items",
+          def.identifier,
+          ".item.json",
+        );
+        return writeJsonFile(path, def);
+      }),
+    );
+
+    await Promise.all(tasks);
+  }
+
+  #createPath(
+    directory: string,
+    identifier: string,
+    suffix: string,
+  ): string {
+    return join(this.rootPath, directory, identifier.split(":")[1] + suffix);
+  }
+}
+
+export class DefinitionSet<T extends BehaviorPackDefinition> extends Set<T> {
+  #seen = new Set<string>();
+  // deno-lint-ignore no-explicit-any
+  #ctor: new (...args: any[]) => T;
+
+  // deno-lint-ignore no-explicit-any
+  constructor(ctor: new (...args: any[]) => T) {
+    super();
+    this.#ctor = ctor;
+  }
+
+  override add(...defs: T[]): this {
+    for (const def of defs) {
+      const id = this.getId(def);
+      if (this.#seen.has(id)) {
+        throw new TypeError(`Duplicate definition for ID: ${id}`);
+      }
+      this.#seen.add(id);
+      super.add(def);
     }
+    return this;
+  }
+
+  override delete(def: T): boolean {
+    const id = this.getId(def);
+    this.#seen.delete(id);
+    return super.delete(def);
+  }
+
+  override clear(): void {
+    this.#seen.clear();
+    super.clear();
+  }
+
+  getId(def: T): string {
+    if (def instanceof this.#ctor) {
+      return def.identifier;
+    }
+    throw new TypeError(
+      `Unknown definition: got ${def.constructor.name}, expected ${this.#ctor.name}`,
+    );
   }
 }
 
@@ -54,8 +121,4 @@ async function writeJsonFile(path: string, data: unknown): Promise<void> {
   const json = JSON.stringify(data, null, 2);
   await ensureDir(dirname(path));
   await Deno.writeTextFile(path, json);
-}
-
-function idToPath(identifier: string): string {
-  return identifier.split(":")[1];
 }
